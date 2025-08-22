4d043fb51ba81476a15df5c68e6ac1ce14e014dd
kanade133 <47049913+kanade133@users.noreply.github.com>
2025-08-22 16:27:47 +0800

    Add more example

--- 4_TestCSharpProject/Test/Program.cs
+++ 4_TestCSharpProject/Test/Program.cs
@@ -4,40 +4,34 @@ namespace Test
 {
     internal class Program
     {
-        static string traderFrontAddress = "tcp://182.254.243.31:40001";
-        static string marketFrontAddress = "tcp://182.254.243.31:40011";
-        static string brokerId = "9999";
-        static string userId = "xxxxxx";
-        static string password = "xxxxxx";
+        private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        private static readonly Config _config = LoadConfig();
 
         static async Task Main(string[] args)
         {
-            //await MarketTest();
-            await TradeTest();
-
+            Console.WriteLine("Input what to do:");
+            Console.WriteLine("1. Market; 2. Trader");
+            string? key = Console.ReadLine();
+            if (key == "1")
+            {
+                await MarketTest();
+            }
+            else if (key == "2")
+            {
+                await TraderTest();
+            }
+            else
+            {
+                Console.WriteLine(" Invalid selection.");
+            }
             Console.WriteLine("End.");
         }
 
         private static async Task MarketTest()
         {
-            Console.WriteLine("Reading config.json");
-
-            string configPath = "config.json";
-            string configDevPath = "config.dev.json";
-            if (File.Exists(configDevPath))
-            {
-                configPath = configDevPath;
-            }
-            var config = JsonSerializer.Deserialize<JsonDocument>(File.ReadAllText(configPath))!;
-            marketFrontAddress = config.RootElement.GetProperty("frontAddress").GetString()!;
-            brokerId = config.RootElement.GetProperty("brokerId").GetString()!;
-            userId = config.RootElement.GetProperty("userId").GetString()!;
-            password = config.RootElement.GetProperty("password").GetString()!;
-            Console.WriteLine(marketFrontAddress);
-            Console.WriteLine(brokerId);
-            Console.WriteLine(userId);
-            Console.WriteLine(password);
-
             Console.WriteLine(CThostFtdcMdApi.GetApiVersion());
 
             var mdSpi = new MyMdSpi();
@@ -48,12 +42,27 @@ namespace Test
             mdSpi.SubscribeMarketData("IF2509");
             await Task.Delay(Timeout.Infinite);
         }
-        private static async Task TradeTest()
+        private static async Task TraderTest()
         {
             Console.WriteLine(CThostFtdcTraderApi.GetApiVersion());
-            var tradeSpi = new MyTradeSpi();
-            tradeSpi.Init();
+            var tradeSpi = new MyTraderSpi();
+            await tradeSpi.Init();
+            Console.WriteLine("Trader API initialized.");
+            await tradeSpi.Login();
+            Console.WriteLine("Trader API login.");
+            await tradeSpi.ReqQryTradingAccount();
             await Task.Delay(Timeout.Infinite);
+        }
+        private static Config LoadConfig()
+        {
+            Console.WriteLine("Reading config.json");
+            string configPath = "config.json";
+            string configDevPath = "config.dev.json";
+            if (File.Exists(configDevPath))
+            {
+                configPath = configDevPath;
+            }
+            return JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath), _jsonSerializerOptions)!;
         }
 
         class MyMdSpi : CThostFtdcMdSpi
@@ -70,7 +79,7 @@ namespace Test
                 System.IO.Directory.CreateDirectory("spi/MD_");
                 _mdApi = CThostFtdcMdApi.CreateFtdcMdApi("spi/MD_");
                 _mdApi.RegisterSpi(this);
-                _mdApi.RegisterFront(marketFrontAddress);
+                _mdApi.RegisterFront(_config.MarketFrontAddress);
                 var task = Request();
                 _mdApi.Init();
                 Console.WriteLine("Initing...");
@@ -81,9 +90,9 @@ namespace Test
                 var task = Request();
                 int? requestCode = _mdApi?.ReqUserLogin(new CThostFtdcReqUserLoginField
                 {
-                    BrokerID = brokerId,
-                    UserID = userId,
-                    Password = password,
+                    BrokerID = _config.BrokerId,
+                    UserID = _config.UserId,
+                    Password = _config.Password,
                 }, ++_requestId);
                 Console.WriteLine($"Request User Login: {requestCode}");
                 if (requestCode == 0)
@@ -189,25 +198,123 @@ namespace Test
                 Console.WriteLine($"Quote response received for instrument: {pForQuoteRsp.InstrumentID}");
             }
         }
-        class MyTradeSpi : CThostFtdcTraderSpi
+        class MyTraderSpi : CThostFtdcTraderSpi
         {
             private CThostFtdcTraderApi? _tradeApi;
+            private int _requestId = 0;
+            private TaskCompletionSource? _tcs;
 
-            public void Init()
+            public Task Init()
             {
                 System.IO.Directory.CreateDirectory("spi/Trade_");
                 _tradeApi = CThostFtdcTraderApi.CreateFtdcTraderApi("spi/Trade_");
                 _tradeApi.RegisterSpi(this);
-                _tradeApi.RegisterFront(traderFrontAddress);
+                _tradeApi.RegisterFront(_config.TraderFrontAddress);
+                var task = Request();
                 _tradeApi.Init();
                 Console.WriteLine("Initing...");
+                return task;
+            }
+            public Task Login()
+            {
+                var task = Request();
+                int? requestCode = _tradeApi?.ReqUserLogin(new CThostFtdcReqUserLoginField
+                {
+                    BrokerID = _config.BrokerId,
+                    UserID = _config.UserId,
+                    Password = _config.Password,
+                }, ++_requestId);
+                Console.WriteLine($"Request User Login: {requestCode}");
+                if (requestCode == 0)
+                {
+                    return task;
+                }
+                else
+                {
+                    Response();
+                    return Task.CompletedTask;
+                }
+            }
+            public Task ReqQryTradingAccount()
+            {
+                var task = Request();
+                var requestCode = _tradeApi?.ReqQryTradingAccount(new CThostFtdcQryTradingAccountField
+                {
+                    BrokerID = _config.BrokerId,
+                    InvestorID = _config.UserId,
+                }, ++_requestId);
+                if (requestCode == 0)
+                {
+                    return task;
+                }
+                else
+                {
+                    Console.WriteLine($"Request Qry Trading Account failed: {requestCode}");
+                    return Task.CompletedTask;
+                }
+            }
+            private Task Request()
+            {
+                if (_tcs != null && !_tcs.Task.IsCompleted)
+                {
+                    return Task.FromException(new InvalidOperationException("Request already in progress."));
+                }
+                else
+                {
+                    _tcs = new TaskCompletionSource();
+                    return _tcs.Task;
+                }
+            }
+            private bool Response()
+            {
+                if (_tcs == null || _tcs.Task.IsCompleted)
+                {
+                    Console.WriteLine("No request in progress.");
+                    return false;
+                }
+                else
+                {
+                    var tcs = _tcs;
+                    _tcs = null;
+                    tcs.SetResult();
+                    return true;
+                }
             }
 
             public override void OnFrontConnected()
             {
                 base.OnFrontConnected();
-                Console.WriteLine("Trade front connected.");
+                Console.WriteLine("Trader front connected.");
+                Response();
             }
+            public override void OnFrontDisconnected(int nReason)
+            {
+                base.OnFrontDisconnected(nReason);
+                Console.WriteLine($"Trader front disconnected. Reason: {nReason}");
+            }
+            public override void OnRspUserLogin(CThostFtdcRspUserLoginField pRspUserLogin, CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast)
+            {
+                base.OnRspUserLogin(pRspUserLogin, pRspInfo, nRequestID, bIsLast);
+                Console.WriteLine($"Trader user login response received. ErrorID: {pRspInfo.ErrorID}, ErrorMsg: {pRspInfo.ErrorMsg}");
+                if (pRspInfo.ErrorID == 0)
+                {
+                    Response();
+                }
+            }
+            public override void OnRspQryTradingAccount(CThostFtdcTradingAccountField pTradingAccount, CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast)
+            {
+                base.OnRspQryTradingAccount(pTradingAccount, pRspInfo, nRequestID, bIsLast);
+                Console.WriteLine($"Trading account query response received. Account: {pTradingAccount.BrokerID} - {pTradingAccount.Available}");
+                Response();
+            }
+        }
+        class Config
+        {
+            public string TraderFrontAddress { get; set; } = "";
+            public string MarketFrontAddress { get; set; } = "";
+            public string BrokerId { get; set; } = "";
+            public string UserId { get; set; } = "";
+            public string Password { get; set; } = "";
         }
     }
 }
